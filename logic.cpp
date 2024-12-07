a2f0100ba3b78fcd2338620e6ce6e73f68ca1905
Rahul Singh <139536574+rahulsingh9063@users.noreply.github.com>
2024-12-07 12:25:51 +0530

    Add files via upload

---
+++ logic.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    if (cin>>n)
+    {
+         cout<<"Rahul"<<endl;
+    }  
+    return 0;
+}
---
+++ solidRectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int rows = 0 ; rows<3; rows++){
+        for(int cols =0; cols<5; cols++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
